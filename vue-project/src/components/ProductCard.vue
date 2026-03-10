73b08175e3acf889ba9444f2e0d199ba507a6ee3
Viktor G. <viktor.gouchet@gmail.com>
2026-03-10 14:39:25 +0100

    commit to check branch routes

--- vue-project/src/components/ProductCard.vue
+++ vue-project/src/components/ProductCard.vue
@@ -1,5 +1,58 @@
-<script></script>
+<script>
+export default {
+    name: 'ProductCard',
+    data() {
+        return {
 
-<template></template>
+        }
+    },
+    methods: {
+        addToCart() {
+            this.$emit('add-to-cart')
+        },
+        // productPage() {
+        //     this.$emit('product-page')
+        // }
+    }
+}
+</script>
 
-<style></style>
+<template>
+    <div class="item-card" @click="productPage" > 
+        <img src={{ product.image }} alt="photography of the product">
+        <div class="product-info">
+            <p>{{ product.name }}</p>
+            <p>{{ product.price }}</p>
+        </div>
+        <div class="cart-add">
+            <button @click="addToCart">+</button>
+        </div>
+    </div>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap');
+
+* {
+    padding: 0;
+    margin: 0;
+    font-family: 'Jost';
+}
+
+.item-card{
+    display: flex;
+    flex-direction: column;
+    padding: 1.3rem 1.6rem;
+}
+
+.product-info {
+    flex-direction: row;
+}
+
+btn{
+    padding: 1.2rem 1.6rem;
+    border: none;
+    border-radius: 16px;
+    background-color: #ccc;
+}
+</style>
