4c40268846dbe0dd10075f3203ea848f4080b8cd
Viktor G. <viktor.gouchet@gmail.com>
2026-03-10 12:25:13 +0100

    routes created

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,8 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/products',
+    name: 'Products',
+    component: Products
+  },
+  {
+    path: '/cart',
+    name: 'Cart',
+    component: Cart
+  }
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: routes
 })
 
 export default router
